69ae1fa9a794b9b816124ea6cf270bfb468b7e87
arvas <av539@live.com>
2012-08-21 14:47:10 +0100

    Added comments for additional clarity

--- execve.s
+++ execve.s
@@ -1,21 +1,22 @@
 #execve.s - by arvas - Read README.md for more info
+#change the indicated values
  .section .data
 args:
- .asciz "root@0.0.0.0"
+ .asciz "root@0.0.0.0" #change
 
  .section .text
  .globl main
 main:
  xorl %eax, %eax
  pushl %eax
- pushl $0x6873732F
- pushl $0x6E69622F
- pushl $0x7273752F
+ pushl $0x6873732F #(hss/) change 
+ pushl $0x6E69622F #(nib/) change 
+ pushl $0x7273752F #(rsu/) change
  movl %esp, %ebx
  pushl %eax
  movl %esp, %edx
  pushl $args
- pushl %edx
+ pushl %edx        #null termination
  movl %esp, %ecx
  movb $11, %al
  int $0x80
